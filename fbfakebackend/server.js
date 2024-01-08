002c377726b20e11f12dec4b68d48e54a1a787fe
Tarho <115691708+Tarho@users.noreply.github.com>
2024-01-08 18:28:13 +0700

    Add files via upload

---
+++ fbfakebackend/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+import Posts from "./postModel.js";
+import Cors from "cors";
+import Pusher from "pusher";
+import dotenv from "dotenv";
+
+//App Config
+const app = express();
+const port = process.env.PORT || 9000;
+const connection_url = "mongodb://localhost:27017";
+dotenv.config();
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: "ap1",
+  useTLS: true,
+});
+//Middleware
+app.use(express.json());
+app.use(Cors());
+
+//DB Config
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("DB Connected");
+});
+//API Endpoints
+app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"));
+app.post("/upload", async (req, res) => {
+  try {
+    const dbPost = req.body;
+    const data = await Posts.create(dbPost);
+    res.status(201).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get("/sync", async (req, res) => {
+  try {
+    const data = await Posts.find().exec();
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+//Listener
+app.listen(port, () => console.log(`Listening on localhost: ${port}`));
